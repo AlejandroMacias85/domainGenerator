cfd2d2ea635a1f5a0810bf7f56fa712fb46eddf9
Luffy85 <macias85ale@gmail.com>
2022-06-11 02:38:10 +0000

    tarea

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let domainGenerator = [];
+  let pronoun = ["the", "our", "a"];
+  let adj = ["great", "big", "old", "silly", "small"];
+  let noun = ["dog", "earth", "beard", "peace", "cartoon"];
+  let domain = [".com", ".net", ".us", ".org"];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < domain.length; l++) {
+          domainGenerator.push(pronoun[i] + adj[j] + noun[k] + domain[l]);
+          console.log(domainGenerator[domainGenerator.length - 1]);
+        }
+      }
+    }
+  }
 };
